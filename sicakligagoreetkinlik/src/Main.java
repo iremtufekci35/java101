16e73ccb5fa59599b4958594bccf56bb6fd5135e
iremtufekci35 <113184262+iremtufekci35@users.noreply.github.com>
2022-09-16 20:14:57 +0300

    sicakliga göre etkinlik önerisi

---
+++ sicakligagoreetkinlik/src/Main.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int heat;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Hava sıcaklığını giriniz: ");
+        heat=input.nextInt();
+
+        if (heat<5)
+        {
+            System.out.println("Kayağa gidebilirsin.");
+        } else if (heat<=25) {
+            if (heat<=15)
+            {
+                System.out.println("Sinemaya gidebilirsin.");
+            }
+            if (heat>=10){
+                System.out.println("Pikniğe gidebilirsin.");
+            }
+        else{
+            System.out.println("Yüzmeye gidebilirsin.");
+            }
+        }
+    }
+}
