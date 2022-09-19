d33b64b6105777938298f21965bf0cd362805886
iremtufekci35 <113184262+iremtufekci35@users.noreply.github.com>
2022-09-19 14:57:38 +0300

    yildizlar_ile ters_ucgen

---
+++ yildizlar_ile_ters_ucgen_odev/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(" Basamak sayısını giriniz : ");
+        int n = input.nextInt();
+
+        for (int i = n; i>= 1; i--) {
+            for (int j=(n-i); j>= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k=(2 * i)-1; k>=1; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
