282c1f7cb288b1b4a6dc448d42702ad98c3975f1
iremtufekci35 <113184262+iremtufekci35@users.noreply.github.com>
2022-09-22 19:32:56 +0300

    dizi

---
+++ dizideki_elemanlar_ort/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+       double list[]={15,6,7,12,7,9,3};
+        double sum=0;
+        double total=1;
+        for(int i=0; i<list.length;i++){
+            sum=1/list[i];
+            total+=sum;
+          
+
+        }
+        System.out.println(total);
+
+    }
+}
